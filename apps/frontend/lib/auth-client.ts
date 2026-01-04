acfcc896b6617d34a1897cc9f74eeb4d7d78f853
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2026-01-04 17:49:44 +0100

    feat: initialize better-auth client and implement sign-in and sign-up routes

---
+++ apps/frontend/lib/auth-client.ts
@@ -0,0 +1,31 @@
+import { createAuthClient } from 'better-auth/react';
+
+export const authClient = createAuthClient({
+  // baseURL: process.env.NEXT_PUBLIC_APP_URL,
+  baseURL: 'http://localhost:3000',
+  fetchOptions: {
+    credentials: 'include',
+  },
+  // plugins: [
+  //   {
+  //     id: 'next-cookies-request',
+  //     fetchPlugins: [
+  //       {
+  //         id: 'next-cookies-request-plugin',
+  //         name: 'next-cookies-request-plugin',
+  //         hooks: {
+  //           async onRequest(ctx) {
+  //             if (typeof window === 'undefined') {
+  //               const { cookies } = await import('next/headers');
+  //               const headers = await cookies();
+  //               ctx.headers.set('cookie', headers.toString());
+  //             }
+  //           },
+  //         },
+  //       },
+  //     ],
+  //   },
+  // ],
+});
+
+export const { signIn, signOut, signUp, useSession, getSession } = authClient;
